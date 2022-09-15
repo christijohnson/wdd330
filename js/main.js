c4ffe4a1587464148e1512e4dbf7b8f36da2efd9
christijohnson <christiajohnson@gmail.com>
2022-09-14 20:53:56 -0600

    added buildlist function

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+const links = [
+    {
+        label: "Week 1 notes",
+        url: "week1/index.html"
+    },
+    {
+        label: "Week 2 notes",
+        url: "week2/index.html"
+    },
+    {
+        label: "Week 3 notes",
+        url: "week3/index.html"
+    },
+];
+
+    function buildList(assignments) {
+        assignments.forEach(assignment => {
+            let week = document.createElement('li');
+            //week.textContent = assignment.label;
+
+            let link = document.createElement('a');
+            link.setAttribute('href', assignment.url);
+            link.textContent = assignment.label;
+
+            week.appendChild(link);
+
+            document.querySelector('ol').appendChild(week);
+        })
+    }
+
+    buildList(links);
