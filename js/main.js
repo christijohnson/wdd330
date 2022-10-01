13b5c0f437f952f7f2f3c15196cb913ae9c4c2ad
christijohnson <christiajohnson@gmail.com>
2022-10-01 17:48:53 -0600

    week 3 notes and examples

--- js/main.js
+++ js/main.js
@@ -12,6 +12,14 @@ const links = [
         label: "Week 3 notes",
         url: "week3/index.html"
     },
+    {
+        label: "Week 4 notes",
+        url: "week4/index.html"
+    },
+    {
+        label: "Week 5 notes",
+        url: "week5/index.html"
+    },
 ];
 
     function buildList(assignments) {
